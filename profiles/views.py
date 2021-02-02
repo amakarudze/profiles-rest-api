fb15c185b5f39f5f486febcd71cea2b9b207c0e7
Anna Makarudze <amakarudze@gmail.com>
2021-02-02 17:30:32 +0200

    Add PUT, PATCH, DELETE methods to APIView

--- profiles/views.py
+++ profiles/views.py
@@ -28,3 +28,16 @@ class HelloAPIView(APIView):
             return Response({'message': message})
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        """Handle updating an object."""
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle a partial update of an object."""
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Delete an object."""
+        return Response({'method': 'DELETE'})
+
